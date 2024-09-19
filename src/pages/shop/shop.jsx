e9f5ee1554b9e3cd2d0e8ed3af2c51e7aff6293d
Jason <mat.lopez072023@gmail.com>
2024-09-19 23:33:41 +0800

    AddedProductComponentInShop

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { PRODUCTS } from '../../products'
+import { Product } from './product'
 
 export const Shop = () => {
   return (
-    <div>shop</div>
+    <div className='shop'>
+        <div className='shop-title'>
+            <h1>Jason Shop</h1>
+        </div>
+        <div className='products'>
+            {PRODUCTS.map((product) => <Product data={product} key={product.id}/>)}</div>
+    </div>
   )
 }
